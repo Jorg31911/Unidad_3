cddd853512b4e873361ed14587ad99d4afa940b8
LAB02-06 <LAB02-06@DESKTOP-7KA7BRS>
2019-03-04 11:43:53 -0600

    Se agrego el primer formulario y la clase estuduiante en carpeta modelo.

---
+++ ActividadesUnidad3/ActividadesUnidad3/Modelo/Estudiante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadesUnidad3.Modelo
+{
+    class Estudiante
+    {
+        public byte edad { get; set; }
+        public string genero { get; set; }
+        public string nombre { get; set; }
+        public long numeroControl { get; set; }
+        public void Estudiar(String texto)
+        {
+            //Metodo para estudiar.
+        }//fin del metodo Estudiar
+        public void HacerTarea(String tema)
+        {
+            //Metodo que hacer tarea
+        }//Fin de metodo hacer tarea
+    }
+}
